0eba760c6cf410d419677b289af7419208cfb253
Molotvan <molotvan@yandex.ru>
2022-04-19 01:10:42 +0300

    ERROR added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         OnTaskDoneListener listener = System.out::println;
-        Worker worker = new Worker(listener);
+        OnTaskErrorListener error = System.out :: println;
+        Worker worker = new Worker(listener, error);
         worker.start();
     }
 }
